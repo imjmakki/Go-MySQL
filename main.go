53740ec675ec510184e4bb614fd41a7113970413
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-02-16 17:59:58 +0300

    start connecting to db

--- main.go
+++ main.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+type Tag struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	fmt.Println("Go MySQL Tutorial")
+
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err := sql.Open("mysql", "root:password1@tcp(127.0.0.1:3306)/test")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
+
+	// perform a db.Query insert
+	insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
+
+	// Execute the query
+	results, err := db.Query("SELECT id, name FROM tags")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	for results.Next() {
+		var tag Tag
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&tag.ID, &tag.Name)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// and then print out the tag's Name attribute
+		log.Printf(tag.Name)
+	}
+
+}
